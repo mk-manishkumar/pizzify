217676268be389beca031291f6a52abcd69b7299
Manish Kumar <manish.login01@gmail.com>
2025-06-25 21:30:11 +0530

    re-initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to Pizzify</h1>
+      <p className="mt-4 text-lg">Your one-stop solution for all pizza needs!</p>
+    </div>
+  )
+}
+
+export default App
