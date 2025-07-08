64f553b88d8e4d234cb850a5901081aeff8c8c24
Manish Kumar <manish.login01@gmail.com>
2025-07-08 23:39:54 +0530

    add redux-persist

---
+++ src/redux/cartSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export type CartItem = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  quantity: number;
+};
+
+type CartState = {
+  cart: CartItem[];
+};
+
+const initialState: CartState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<CartItem>) => {
+      const itemInCart = state.cart.find(item => item.id === action.payload.id);
+      if (itemInCart) {
+        itemInCart.quantity += action.payload.quantity;
+      } else {
+        state.cart.push({ ...action.payload });
+      }
+    },
+
+    incrementQuantity: (state, action: PayloadAction<number>) => {
+      const item = state.cart.find(item => item.id === action.payload);
+      if (item) item.quantity += 1;
+    },
+
+    decrementQuantity: (state, action: PayloadAction<number>) => {
+      const item = state.cart.find(item => item.id === action.payload);
+      if (item && item.quantity > 1) item.quantity -= 1;
+    },
+
+    removeItem: (state, action: PayloadAction<number>) => {
+      state.cart = state.cart.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const {
+  addToCart,
+  incrementQuantity,
+  decrementQuantity,
+  removeItem,
+} = cartSlice.actions;
+
+export const cartReducer = cartSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cartReducer } from "./cartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer
+  },
 });
